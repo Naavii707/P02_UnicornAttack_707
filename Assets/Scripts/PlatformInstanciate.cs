bdcf631aecad28d5efa29032c1f7e18151debb11
Naavii <156258057+Naavii707@users.noreply.github.com>
2025-03-21 01:01:45 -0600

    UA_V02

--- Assets/Scripts/PlatformInstanciate.cs
+++ Assets/Scripts/PlatformInstanciate.cs
@@ -1,15 +1,20 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformInstantiate : MonoBehaviour
 {
     [SerializeField]
-    private List<GameObject> platforms;
+    private List<GameObject> platforms; // Lista de plataformas disponibles
     [SerializeField]
-    private float distanceBetweenPlatforms = 2f;
+    private GameObject[] safePlatforms; // Las plataformas seguras que no matan al jugador
     [SerializeField]
-    private int initialPlatforms = 10;
+    private float distanceBetweenPlatforms = 2f; // Distancia entre plataformas
+    [SerializeField]
+    private int initialPlatforms = 10; // Número de plataformas a instanciar
     private float offsetPositionX = 0f;
+
+    private int platformsInstanciadas = 0; // Contador de plataformas instanciadas
 
     public void Start()
     {
@@ -21,15 +26,32 @@ public class PlatformInstantiate : MonoBehaviour
     {
         for (int i = 0; i < amount; i++)
         {
-            int randomIndex = Random.Range(0, platforms.Count);
+            // Elegimos una plataforma aleatoria de la lista
+            int randomIndex = UnityEngine.Random.Range(0, platforms.Count);
+
             if (offsetPositionX != 0)
             {
                 offsetPositionX += platforms[randomIndex].GetComponent<BoxCollider>().size.x * 0.5f;
             }
+
+            // Instanciamos la plataforma
             GameObject platform = Instantiate(platforms[randomIndex], Vector3.zero, Quaternion.identity);
             offsetPositionX += distanceBetweenPlatforms + platform.GetComponent<BoxCollider>().size.x * 0.5f;
             platform.transform.SetParent(transform);
             platform.transform.localPosition = new Vector3(offsetPositionX, 0, 0);
+
+            // Comprobamos si es una de las primeras dos plataformas
+            if (platformsInstanciadas < 2)
+            {
+                // Verificamos si la plataforma es una de las seguras
+                if (Array.Exists(safePlatforms, safePlatform => safePlatform.name == platform.name))
+                {
+                    // Desactivar el Collider para que no cause daño
+                    platform.GetComponent<Collider>().isTrigger = true;
+                }
+            }
+
+            platformsInstanciadas++;
         }
     }
 
@@ -39,6 +61,8 @@ public class PlatformInstantiate : MonoBehaviour
         {
             Destroy(child.gameObject);
         }
+        platformsInstanciadas = 0; // Resetear contador al reiniciar
         Start();
     }
 }
+
